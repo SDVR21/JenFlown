066c811fd2083dd86b37ae0e5abfb68a184a1b21
Daeun Kim <63909661+SDVR21@users.noreply.github.com>
2023-12-06 10:05:03 +0900

    Update b_init_users.sql

--- mysql/initdb.d/b_init_users.sql
+++ mysql/initdb.d/b_init_users.sql
@@ -1,10 +1,10 @@
 create user 'root'@'%' IDENTIFIED BY 'root';
 grant all privileges ON *.* TO 'root'@'%' with grant option;
-CREATE USER 'sungshin'@'172.18.0.3' IDENTIFIED BY 'tjdtls!';
-CREATE USER 'sungshin'@'172.18.0.2' IDENTIFIED BY 'tjdtls!';
-CREATE USER 'sungshin'@'172.18.0.1' IDENTIFIED BY 'tjdtls!';
-grant all privileges on FLOWN.* to 'sungshin'@'172.18.0.3';
-grant all privileges on FLOWN.* to 'sungshin'@'172.18.0.2';
-grant all privileges on FLOWN.* to 'sungshin'@'172.18.0.1';
+CREATE USER 'sungshin'@'172.22.0.3' IDENTIFIED BY 'tjdtls!';
+CREATE USER 'sungshin'@'172.22.0.2' IDENTIFIED BY 'tjdtls!';
+CREATE USER 'sungshin'@'172.22.0.1' IDENTIFIED BY 'tjdtls!';
+grant all privileges on FLOWN.* to 'sungshin'@'172.22.0.3';
+grant all privileges on FLOWN.* to 'sungshin'@'172.22.0.2';
+grant all privileges on FLOWN.* to 'sungshin'@'172.22.0.1';
 drop user 'root'@'%';
 FLUSH PRIVILEGES;
